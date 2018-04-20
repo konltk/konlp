a81cb43769badb4e48a6b995175791aa22f80ff6
younghuncho <cyh905@nate.com>
2018-04-20 17:26:18 +0900

    add dictionary files

---
+++ konlp/kma/klt_data/lib/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Korean Natural Language Toolkit: 
+#
+# 
+# Author:  <@gmail.com>
+#
+# Contributors: 
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
