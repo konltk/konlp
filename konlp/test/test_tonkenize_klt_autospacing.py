f0e9779d34717ad4bfba3259edc26cfa192f60ea
younghuncho <cyh905@nate.com>
2018-05-03 22:34:04 +0900

    test KltAsp code

--- konlp/test/test_tonkenize_klt_autospacing.py
+++ konlp/test/test_tonkenize_klt_autospacing.py
@@ -9,9 +9,10 @@
 # For license information, see LICENSE.TXT
 # ========================================================
 import pytest
-from konlp.tokenize import klt_asp
+from konlp.tokenize import KltAsp
 
 def test_asp():
-    assert klt_asp(text="국민대학교자연어처리연구실") == ['국민대학교', '자연어처리', '연구실']
-    assert klt_asp(text="국민대학교자연어처리연구실", split=False) == '국민대학교 자연어처리 연구실'
-    assert klt_asp(text="국민대학교자연어처리연구실", split=True) == ['국민대학교', '자연어처리', '연구실']
+    k = KltAsp()
+    assert k.asp(text="국민대학교자연어처리연구실") == ['국민대학교', '자연어처리', '연구실']
+    assert k.asp(text="국민대학교자연어처리연구실", split=False) == '국민대학교 자연어처리 연구실'
+    assert k.asp(text="국민대학교자연어처리연구실", split=True) == ['국민대학교', '자연어처리', '연구실']
