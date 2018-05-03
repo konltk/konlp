f41e284f1927a08c2a30ccc56ce4593c95a8569b
hyunyoung2 <le32146@gmail.com>
2018-05-03 15:59:32 +0900

    Delete __init__.py
    We don't need this file in here

--- konlp/kma/klt/data/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Korean Natural Language Toolkit: 
-#
-# 
-# Author:  <@gmail.com>
-#
-# Contributors: 
-# URL: <http://nltk.org/>
-# For license information, see LICENSE.TXT
