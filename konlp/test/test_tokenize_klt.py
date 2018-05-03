21c72ba709a58ed5bb0ea9cf338e4920a8ad2d3a
younghuncho <cyh905@nate.com>
2018-05-03 18:29:56 +0900

    tokenize klt asp test file

---
+++ konlp/test/test_tokenize_klt.py
@@ -0,0 +1,17 @@
+# -*- coding:utf8 -*-
+# Copyright (C) 2017 - 0000 KoNLTK project
+#
+# Klt 형태소 분석기 for Korean Natural Language Toolkit
+#
+# Author: Younghun Cho <cyh905@gmail.com>
+#         Seungshik Kang <sskang@kookmin.ac.kr>
+# URL: <https://www.konltk.org>
+# For license information, see LICENSE.TXT
+# ========================================================
+import pytest
+from konlp.tokenize import klt_asp
+
+def test_asp():
+    assert klt_asp(text="국민대학교자연어처리연구실")
+    assert klt_asp(text="국민대학교자연어처리연구실", split=False)
+    assert klt_asp(text="국민대학교자연어처리연구실", split=True)
