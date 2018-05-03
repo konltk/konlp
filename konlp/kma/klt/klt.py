c0c24deff3ecfc5a75a678c04c68226390986c94
hyunyoung2 <le32146@gmail.com>
2018-05-03 16:25:45 +0900

    Update klt.py

--- konlp/kma/klt/klt.py
+++ konlp/kma/klt/klt.py
@@ -3,7 +3,7 @@
 # Klt 형태소 분석기 for Korean Natural Language Toolkit
 #
 # Author: Younghun Cho <cyh905@gmail.com>
-#         Hyunyoung Lee <hyun02.engineer.com>
+#         Hyunyoung Lee <hyun02.engineer@gmail.com>
 #         Seungshik Kang <sskang@kookmin.ac.kr>
 # URL: <https://www.konltk.org>
 # For license information, see LICENSE.TXT
