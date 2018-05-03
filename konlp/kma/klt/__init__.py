62a4006b347236386c842fda8f9e87d80d04c2a9
hyunyoung2 <le32146@gmail.com>
2018-05-03 16:37:52 +0900

    Update __init__.py
    change the comment

--- konlp/kma/klt/__init__.py
+++ konlp/kma/klt/__init__.py
@@ -1,11 +1,12 @@
 # Copyright (C) 2017 - 0000 KoNLTK project
 #
-# Klt 형태소 분석기 for Korean Natural Language Toolkit
+# Korean Natural Language Toolkit: Korean morpheme analyzer - klt
 #
-# Author: Younghun Cho <cyh905@gmail.com>
+#
+# Author: HyunYoung Lee <hyun02.engineer@gmail.com>
+#         Younghun Cho <cyh905@gmail.com>
 #         Seungshik Kang <sskang@kookmin.ac.kr>
 # URL: <https://www.konltk.org>
 # For license information, see LICENSE.TXT
 # ========================================================
-
 # from konlp.kma import klt
