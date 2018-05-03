9249ab7db2eb4f0e774359e7948479ded18beff8
hyunyoung2 <le32146@gmail.com>
2018-05-03 19:34:21 +0900

    Update __init__.py

--- konlp/tokenize/klt/__init__.py
+++ konlp/tokenize/klt/__init__.py
@@ -3,6 +3,7 @@
 # Klt tokenizer for Korean Natural Language Toolkit
 #
 # Author: Younghun Cho <cyh905@gmail.com>
+#         HyunYoung Lee <hyun02.engineer@gmail.com>
 #         Seungshik Kang <sskang@kookmin.ac.kr>
 # URL: <https://www.konltk.org>
 # For license information, see LICENSE.TXT
