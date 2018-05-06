138f9802815e5e51527def51fa79c92f23565824
hyunyoung2 <hyun02.engineer@gmail.com>
2018-05-06 23:14:02 +0900

    setting for KoNLP web

---
+++ web/README.md
@@ -0,0 +1,18 @@
+# How to make documentaion of konlp
+
+First, Normally use like the command, "sphinx-build -b html sourcedir builddir"
+
+But in our case, we have to run like this :
+
+> sphinx-apidoc -f -o \_source ../konlp
+
+Finally, run the following to get html files :
+
+> make html
+
+
+
+# Reference 
+
+ - [Sphinx-doc quickstart](http://www.sphinx-doc.org/en/master/usage/quickstart.html)
+ - [How to use google doc style with sphinx extension](http://www.sphinx-doc.org/en/master/ext/napoleon.html)
--- web/conf.py
+++ web/conf.py
@@ -16,11 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# for Docs setup
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
 # -- Project information -----------------------------------------------------
 
 project = 'KoNLTK KoNLP'
@@ -82,6 +77,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
+
+# A list of ignored prefixes for module index sorting.
+modindex_common_prefix = ['konlp.']
 
 
 # -- Options for HTML output -------------------------------------------------
