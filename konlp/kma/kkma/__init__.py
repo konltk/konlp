b731dd4deac9bbbfb8d4c651f2925f066c04ef5e
younghuncho <cyh905@nate.com>
2018-05-10 12:39:20 +0900

    added kma/kkma package

---
+++ konlp/kma/kkma/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2017 - 0000 KoNLTK project
+#
+# Korean Natural Language Toolkit: Korean morpheme analyzer - kkma
+#
+#
+# Author: Younghun Cho <cyh905@gmail.com>
+#
+# URL: <https://www.konltk.org>
+# For license information, see LICENSE.TXT
+# ========================================================
+from konlp.kma.kkma.kkma import Kkma
