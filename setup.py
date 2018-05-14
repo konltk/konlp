6d9be3a6d8f019dc9c3186197530ef42c4ee9c4e
wlsxo1119 <wlsxo1119@naver.com>
2018-05-14 16:19:10 +0900

    idx_extractor_commit

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ VERSION_FILE = os.path.join(os.path.dirname(__file__), 'konlp', 'VERSION')
 with open(VERSION_FILE) as vfh:
     KONLP_VERSION = vfh.read().strip()
 
+#print(find_packages(),'!!')
 # Setuptools
 setup(
     name="konlp",
@@ -68,11 +69,29 @@ Korean natural Language precessing. KoNLP currently requires Python 3.5.""",
     'kma/klt/data/hdic/*',
     # tokenize/klt 모듈의 사전 파일 위치
     'tokenize/klt/data/dic/*',
-    ]}, # Later on we have to change it
-    install_requires=[], # Later on, we have to select
+    # chat/indexer_extractor 모듈의 resource 파일 위치
+    'chat/indexer_extractor/resource/morpheme/binaryDic/*',
+    'chat/indexer_extractor/resource/morpheme/hmmModel/*',
+    'chat/indexer_extractor/resource/morpheme/space/*',
+    'chat/indexer_extractor/utils/gru_crf_NER/*',
+    'chat/indexer_extractor/utils/gru_crf_NER/model_new/*',
+    'chat/indexer_extractor/preprocess/data/*',
+    # chat/pseudo_morpeme_analyzer 모듈의 사전 파일 위치
+    'chat/pseudo_morpeme_analyzer/data/*',
+    # chat/pseudo_morpeme_analyzer 모듈 모델 위치
+    'chat/pseudo_morpeme_analyzer/model_for_graph/*'
+    ]},
+
+    # Later on we have to change it
+    setup_requires=['pytest', 'numpy', 'Cython==0.25.2'],
+    # Later on we have to change it
+    install_requires=['tensorflow==1.4', 'jnius==1.1.0'],
+    # Later on, we have to select
+
     packages=find_packages(), # later on, with exclude setting
     extras_require={}, # Later on, refer to k-NLTK resource
     python_requires='>=3', # Later on, we change this to classifier above
     zip_safe=False, # Later on, we look for exact reason
     # Later on, We have to search for additional condition from Python
+
     )
