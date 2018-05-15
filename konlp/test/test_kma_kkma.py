2f6e897c0895fe45e8c6116b09e561e19112c892
hyunyoung2 <le32146@gmail.com>
2018-05-15 17:27:23 +0900

    Update test_kma_kkma.py
    Change the py.test

--- konlp/test/test_kma_kkma.py
+++ konlp/test/test_kma_kkma.py
@@ -13,18 +13,20 @@
 import pytest
 from konlp.kma.kkma import kkma
 
-simple_str = "안녕하세요. 국민대학교 자연어처리 연구실입니다."
+@pytest.fixture
+def input_str()
+    return "안녕하세요. 국민대학교 자연어처리 연구실입니다."
 
 @pytest.fixture
 def obj():
     kk = kkma.Kkma()
     return kk
 
-def test_tokens(obj, simple_str):
-    assert obj.tokens(simple_str) == ['안녕', '하', '세요', '.', '국민대학교', '자연어', '처리', '연구실', '이', 'ㅂ니다', '.']
+def test_tokens(obj, input_str):
+    assert obj.tokens(input_str) == ['안녕', '하', '세요', '.', '국민대학교', '자연어', '처리', '연구실', '이', 'ㅂ니다', '.']
 
-def test_nouns(obj, simple_str):
-    assert obj.nouns(simple_str) == ['안녕', '국민', '국민대학교', '대학교', '자연어', '자연어처리', '처리', '연구실']
+def test_nouns(obj, input_str):
+    assert obj.nouns(input_str) == ['안녕', '국민', '국민대학교', '대학교', '자연어', '자연어처리', '처리', '연구실']
 
-def test_analyze(obj, simple_str):
-    assert obj.analyze(simple_str) == [['안녕하세요.', ['안녕/NNG', '하/XSV', '세요/EFN', './SF']], ['국민대학교 자연어처리 연구실입니다.', ['국민대학교/NNG', '자연어/NNG', '처리/NNG', '연구실/NNG', '이/VCP', 'ㅂ니다/EFN', './SF']]]
+def test_analyze(obj, input_str):
+    assert obj.analyze(input_str) == [['안녕하세요.', ['안녕/NNG', '하/XSV', '세요/EFN', './SF']], ['국민대학교 자연어처리 연구실입니다.', ['국민대학교/NNG', '자연어/NNG', '처리/NNG', '연구실/NNG', '이/VCP', 'ㅂ니다/EFN', './SF']]]
