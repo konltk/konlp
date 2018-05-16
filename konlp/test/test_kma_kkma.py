dcaf86ee45e10b8d6b73be241d8ba74f3a5ce47d
hyunyoung2 <le32146@gmail.com>
2018-05-16 13:12:19 +0900

    Update test_kma_kkma.py

--- konlp/test/test_kma_kkma.py
+++ konlp/test/test_kma_kkma.py
@@ -11,22 +11,22 @@
 # For license information, see LICENSE.TXT
 # ========================================================
 import pytest
-from konlp.kma.kkma import kkma
+from konlp.kma import Kkma
 
 @pytest.fixture
-def string():
+def input_string():
     return "안녕하세요. 국민대학교 자연어처리 연구실입니다."
 
 @pytest.fixture
 def kkma_instance():
-    kk = kkma.Kkma()
+    kk = Kkma()
     return kk
 
-def test_tokens(kkma_instance, string):
-    assert kkma_instance.tokens(string) == ['안녕', '하', '세요', '.', '국민대학교', '자연어', '처리', '연구실', '이', 'ㅂ니다', '.']
+def test_tokens(kkma_instance, input_string):
+    assert kkma_instance.tokens(input_string) == ['안녕', '하', '세요', '.', '국민대학교', '자연어', '처리', '연구실', '이', 'ㅂ니다', '.']
 
-def test_nouns(kkma_instance, string):
-    assert kkma_instance.nouns(string) == ['안녕', '국민', '국민대학교', '대학교', '자연어', '자연어처리', '처리', '연구실']
+def test_nouns(kkma_instance, input_string):
+    assert kkma_instance.nouns(input_string) == ['안녕', '국민', '국민대학교', '대학교', '자연어', '자연어처리', '처리', '연구실']
 
-def test_analyze(kkma_instance, string):
-    assert kkma_instance.analyze(string) == [['안녕하세요.', ['안녕/NNG', '하/XSV', '세요/EFN', './SF']], ['국민대학교 자연어처리 연구실입니다.', ['국민대학교/NNG', '자연어/NNG', '처리/NNG', '연구실/NNG', '이/VCP', 'ㅂ니다/EFN', './SF']]]
+def test_analyze(kkma_instance, input_string):
+    assert kkma_instance.analyze(input_string) == [['안녕하세요.', ['안녕/NNG', '하/XSV', '세요/EFN', './SF']], ['국민대학교 자연어처리 연구실입니다.', ['국민대학교/NNG', '자연어/NNG', '처리/NNG', '연구실/NNG', '이/VCP', 'ㅂ니다/EFN', './SF']]]
