5b34eba5943092eb73a164975b7097d49a73f4b8
ngh <ngh3053@gmail.com>
2018-05-30 19:25:49 +0900

    konlp-45 kmu automatic word spacing

---
+++ konlp/tokenize/kmuasp/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+# Copyright (C) 2017 - 0000 KoNLTK project
+#
+# Korean Natural Language Toolkit: Korean automatic word spacing
+#
+#
+# Author: GyuHyeon Nam <ngh3053@gmail.com>
+#
+# URL: <https://www.konltk.org>
+# For license information, see LICENSE.TXT
+# ========================================================
+"""korean automatic word spacing
+"""
+
+from konlp.tokenize.kmuasp.kmuasp import KmuAsp
