2c1a42359a9f7aef95c9d75be647e16ffd7fdf55
wlsxo1119 <37436036+wlsxo1119@users.noreply.github.com>
2018-06-05 16:20:48 +0900

    Update test_idx_extractor.py

--- konlp/test/test_idx_extractor.py
+++ konlp/test/test_idx_extractor.py
@@ -4,19 +4,20 @@ import pytest
 from konlp.kma.indexer_extractor import IndexerExtractor
 
 extractor = IndexerExtractor()
-@pytest.fixture
+
+
 def test_analyze():
     str = "밥이 먹고 싶다."
     result = extractor.analyze(str)
     assert result == ['밥/NNG', '먹/VV', 'M#34']
 
-@pytest.fixture
-def test_tokens():
+
+    def test_tokens():
     str = "밥이 먹고 싶다."
     result = extractor.tokens(str)
     assert result == ['밥/NNG', '먹/VV', '고/EC 싶/VX']
 
-@pytest.fixture
+    
 def test_nouns():
     str = "밥이 먹고 싶다."
     result = extractor.nouns(str)
