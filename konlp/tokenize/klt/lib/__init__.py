ad474a25732b718f8fe660d0fab520fbfa4f11c9
wlsxo1119 <wlsxo1119@naver.com>
2018-06-05 11:36:38 +0900

    indexer_extractor, pseudo morpheme analyzer package init

---
+++ konlp/tokenize/klt/lib/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2017 - 0000 KoNLTK project
+#
+# Klt tokenizer for Korean Natural Language Toolkit
+#
+# Author: Younghun Cho <cyh905@gmail.com>
+#         Seungshik Kang <sskang@kookmin.ac.kr>
+# URL: <https://www.konltk.org>
+# For license information, see LICENSE.TXT
+# ========================================================
